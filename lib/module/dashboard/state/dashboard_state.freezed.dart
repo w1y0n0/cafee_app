bf90c45538e1324a0103682c2262d213e04e3d47
l4bs1 <labsi01pnc@gmail.com>
2023-11-22 16:00:27 +0700

    update halaman home

--- lib/module/dashboard/state/dashboard_state.freezed.dart
+++ lib/module/dashboard/state/dashboard_state.freezed.dart
@@ -18,6 +18,8 @@ final _privateConstructorUsedError = UnsupportedError(
 mixin _$DashboardState {
   int get counter => throw _privateConstructorUsedError;
   set counter(int value) => throw _privateConstructorUsedError;
+  List<dynamic> get products => throw _privateConstructorUsedError;
+  set products(List<dynamic> value) => throw _privateConstructorUsedError;
 
   @JsonKey(ignore: true)
   $DashboardStateCopyWith<DashboardState> get copyWith =>
@@ -30,7 +32,7 @@ abstract class $DashboardStateCopyWith<$Res> {
           DashboardState value, $Res Function(DashboardState) then) =
       _$DashboardStateCopyWithImpl<$Res, DashboardState>;
   @useResult
-  $Res call({int counter});
+  $Res call({int counter, List<dynamic> products});
 }
 
 /// @nodoc
@@ -47,12 +49,17 @@ class _$DashboardStateCopyWithImpl<$Res, $Val extends DashboardState>
   @override
   $Res call({
     Object? counter = null,
+    Object? products = null,
   }) {
     return _then(_value.copyWith(
       counter: null == counter
           ? _value.counter
           : counter // ignore: cast_nullable_to_non_nullable
               as int,
+      products: null == products
+          ? _value.products
+          : products // ignore: cast_nullable_to_non_nullable
+              as List<dynamic>,
     ) as $Val);
   }
 }
@@ -65,7 +72,7 @@ abstract class _$$DashboardStateImplCopyWith<$Res>
       __$$DashboardStateImplCopyWithImpl<$Res>;
   @override
   @useResult
-  $Res call({int counter});
+  $Res call({int counter, List<dynamic> products});
 }
 
 /// @nodoc
@@ -80,12 +87,17 @@ class __$$DashboardStateImplCopyWithImpl<$Res>
   @override
   $Res call({
     Object? counter = null,
+    Object? products = null,
   }) {
     return _then(_$DashboardStateImpl(
       counter: null == counter
           ? _value.counter
           : counter // ignore: cast_nullable_to_non_nullable
               as int,
+      products: null == products
+          ? _value.products
+          : products // ignore: cast_nullable_to_non_nullable
+              as List<dynamic>,
     ));
   }
 }
@@ -93,15 +105,141 @@ class __$$DashboardStateImplCopyWithImpl<$Res>
 /// @nodoc
 
 class _$DashboardStateImpl implements _DashboardState {
-  _$DashboardStateImpl({this.counter = 0});
+  _$DashboardStateImpl(
+      {this.counter = 0,
+      this.products = const [
+        {
+          "id": 1,
+          "photo":
+              "https://i.ibb.co/dG68KJM/photo-1513104890138-7c749659a591-crop-entropy-cs-tinysrgb-fit-max-fm-jpg-ixid-Mnwy-ODA4-ODh8-MHwxf-H.jpg",
+          "product_name": "Frenzy Pizza",
+          "price": 25,
+          "category": "Food",
+          "description":
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+          "discount_price": 20,
+          "is_favorite": false
+        },
+        {
+          "id": 2,
+          "photo":
+              "https://i.ibb.co/mHtmhmP/photo-1521305916504-4a1121188589-crop-entropy-cs-tinysrgb-fit-max-fm-jpg-ixid-Mnwy-ODA4-ODh8-MHwxf-H.jpg",
+          "product_name": "Beef Burger",
+          "price": 22,
+          "category": "Food",
+          "description":
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+          "discount_price": 18,
+          "is_favorite": true
+        },
+        {
+          "id": 3,
+          "photo":
+              "https://images.unsplash.com/photo-1625869016774-3a92be2ae2cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+          "product_name": "Seperait",
+          "price": 33,
+          "category": "Drink",
+          "description":
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+          "discount_price": 28,
+          "is_favorite": false
+        },
+        {
+          "id": 4,
+          "photo":
+              "https://images.unsplash.com/photo-1578160112054-954a67602b88?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+          "product_name": "Fried Rice",
+          "price": 31,
+          "category": "Food",
+          "description":
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+          "discount_price": 25,
+          "is_favorite": true
+        },
+        {
+          "id": 5,
+          "photo":
+              "https://i.ibb.co/mHCx9Nj/photo-1517487881594-2787fef5ebf7-crop-entropy-cs-tinysrgb-fit-max-fm-jpg-ixid-Mnwy-ODA4-ODh8-MHwxf-H.jpg",
+          "product_name": "Terrano Milk",
+          "price": 32,
+          "category": "Drink",
+          "description":
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+          "discount_price": 28,
+          "is_favorite": true
+        },
+        {
+          "id": 6,
+          "photo":
+              "https://images.unsplash.com/photo-1626082927389-6cd097cdc6ec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+          "product_name": "Fried Chicken",
+          "price": 49,
+          "category": "Food",
+          "description":
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+          "discount_price": 45,
+          "is_favorite": false
+        },
+        {
+          "id": 7,
+          "photo":
+              "https://images.unsplash.com/photo-1525385133512-2f3bdd039054?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=685&q=80",
+          "product_name": "Mango Juice",
+          "price": 62,
+          "category": "Drink",
+          "description":
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+          "discount_price": 56,
+          "is_favorite": false
+        },
+        {
+          "id": 8,
+          "photo":
+              "https://images.unsplash.com/photo-1600271886742-f049cd451bba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+          "product_name": "Orange Juice",
+          "price": 56,
+          "category": "Drink",
+          "description":
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+          "discount_price": 50,
+          "is_favorite": true
+        },
+        {
+          "id": 9,
+          "photo":
+              "https://images.unsplash.com/photo-1604085792782-8d92f276d7d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80",
+          "product_name": "Avocado Juice",
+          "price": 56,
+          "category": "Drink",
+          "description":
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+          "discount_price": 50,
+          "is_favorite": true
+        },
+        {
+          "id": 10,
+          "photo":
+              "https://images.unsplash.com/photo-1622240506921-042a4e71c172?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+          "product_name": "Puncak Coffe",
+          "price": 56,
+          "category": "Drink",
+          "description":
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+          "discount_price": 50,
+          "is_favorite": true
+        }
+      ]});
 
   @override
   @JsonKey()
   int counter;
+  @override
+  @JsonKey()
+  List<dynamic> products;
 
   @override
   String toString() {
-    return 'DashboardState(counter: $counter)';
+    return 'DashboardState(counter: $counter, products: $products)';
   }
 
   @JsonKey(ignore: true)
@@ -113,11 +251,15 @@ class _$DashboardStateImpl implements _DashboardState {
 }
 
 abstract class _DashboardState implements DashboardState {
-  factory _DashboardState({int counter}) = _$DashboardStateImpl;
+  factory _DashboardState({int counter, List<dynamic> products}) =
+      _$DashboardStateImpl;
 
   @override
   int get counter;
   set counter(int value);
+  @override
+  List<dynamic> get products;
+  set products(List<dynamic> value);
   @override
   @JsonKey(ignore: true)
   _$$DashboardStateImplCopyWith<_$DashboardStateImpl> get copyWith =>
